3825b53dd8b5fc83992fdfdd958405331d17eb0a
arno-3 <arno3nel@gmail.com>
2025-06-09 10:22:26 +0200

    Update gamewindow.cpp

--- TowerDefencePractical/gamewindow.cpp
+++ TowerDefencePractical/gamewindow.cpp
@@ -7,7 +7,7 @@ gamewindow::gamewindow(QWidget *parent, QMainWindow *menu) : QMainWindow(parent)
 
 
     //Create background and initial state of GameWindow
-    setFixedSize(1200,600);
+    setFixedSize(1200,800);
     setStyleSheet("background-color: #5598BF;");
 
     RegT = new TowerBtn(this,0);
@@ -40,20 +40,20 @@ void gamewindow::createMap()
 void gamewindow::convertIsometric()
 {
     QVector<QVector<QLabel*>> grassVector(10, QVector<QLabel*>(10));
-    int sWidth=500;
-    int sHeight = 125;
 
     for (int i = 0; i < 10; ++i)
     {
         for (int j = 0; j < 10; ++j)
         {
             gridVector[i][j] = new gridBlocks(this);
+
+            // Isometric transformation
             int tileWidth = gridVector[i][j]->width();
             int tileHeight = gridVector[i][j]->height();
 
             // Isometric coordinates
-            int x = (j - i) * tileWidth/2;
-            int y = (i + j) * (tileHeight)/4;
+            int x = (j - i) * tileWidth / 2;
+            int y = (i + j) * (tileHeight+9) / 4;
 
             // Center the grid on the screen
             int xOffset = 600;
@@ -63,22 +63,25 @@ void gamewindow::convertIsometric()
             gridVector[i][j]->move(x + xOffset, y + yOffset);
             gridVector[i][j]->setAttribute(Qt::WA_TranslucentBackground); // Ensure transparency
             gridVector[i][j]->setStyleSheet("background: transparent;"); // Remove any back
-            //gridVector[i][j]->show();
+            gridVector[i][j]->show();
 
-//            QPixmap *grass = new QPixmap();
-//            grass->load(":/GrassTile.png");
-//            grass->scaled(sWidth,sHeight,Qt::KeepAspectRatio, Qt::SmoothTransformation);
-
+            // Create and position grass label
+            QPixmap p(":/GrassTile.png");
             grassVector[i][j] = new QLabel(this);
-//            grassVector[i][j]->setPixmap(*grass);
 
-            grassVector[i][j]->setFixedSize(sWidth,sHeight);
-            grassVector[i][j]->setStyleSheet("image: url(:/GrassTile.png);");
-            grassVector[i][j]->move(x+xOffset,y+yOffset);
+            int scaledWidth = 500;  // Desired width
+            int scaledHeight = 125; // Desired height
+
+            QPixmap scaledPixmap = p.scaled(scaledWidth, scaledHeight, Qt::KeepAspectRatio, Qt::SmoothTransformation);
+
+            grassVector[i][j]->setFixedSize(scaledWidth, scaledHeight);
+
+            grassVector[i][j]->setPixmap(scaledPixmap);
+            grassVector[i][j]->setAttribute(Qt::WA_TranslucentBackground); // Ensure transparency
+            grassVector[i][j]->setStyleSheet("background: transparent;"); // Remove any background
+            grassVector[i][j]->move(x + xOffset, y + yOffset);
             grassVector[i][j]->show();
-
         }
-
     }
 
 }
